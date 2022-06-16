74d56112fa8576c97e649240dc1748eab46ee72f
DiffuseHyperion <xieyingjie08@gmail.com>
2022-06-16 09:43:15 +0800

    setupWorld now sets spawn location at 0.5 rather than 0
    also platform increased to 5x5

--- src/main/java/tk/yjservers/gamemaster/GameWorld.java
+++ src/main/java/tk/yjservers/gamemaster/GameWorld.java
@@ -145,9 +145,9 @@ public class GameWorld {
         for (int i = 255; true; i = i - 1) {
             if (!Objects.equals(world.getBlockAt(0, i, 0).getType(), Material.AIR)) {
                 if (setupSpawnPlatform && Objects.equals(world.getBlockAt(0, i, 0).getType(), Material.WATER)) {
-                    fillBlocks(new Location(world, -1, i, -1), new Location(world, 1, i, 1), Material.DIRT);
+                    fillBlocks(new Location(world, -2, i, -2), new Location(world, 2, i, 2), Material.DIRT);
                 }
-                world.setSpawnLocation(0, i + 1, 0);
+                world.setSpawnLocation(new Location(world, 0.5, i + 1, 0.5));
                 break;
             }
         }
